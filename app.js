667655dba0adb1fd89c6eddb1f9a320582e2ed9e
WillyMaikowski <willy.maikowski@gmail.com>
2015-12-08 14:37:33 -0300

    update

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var bodyParser 	= require( 'body-parser' );
 var ugpio				= require( './ugpio' );
 
 var app = express();
-app.use( logger('dev') );
+app.disable( 'x-powered-by' );
+
+app.use( logger( 'dev' ) );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: false } ) );
 
 app.get( '/sms', function( req, res, next ) {
-	res.send( 'Hello World!' );
+	res.send( req.query );
 } );
 
 app.use( function( req, res, next ) {
@@ -20,7 +22,7 @@ app.use( function( req, res, next ) {
 
 app.use( function( err, req, res, next ) {
   res.status( err.status || 500 );
-  res.render( 'error', {
+  res.send( {
     message: err.message,
     error: err //{}
   } );
